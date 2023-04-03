b2f0fe16faaf80cccf5c62d1efcb484378285378
AlexandraTYshchenko <oleksandratisenko59@gmail.com>
2023-04-03 20:24:59 +0300

    hometask 3.2

---
+++ HomeTask3/hometask.3.2/Strings/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Strings;
+StringClass str = new StringClass("Hello World Hello World World");
+int? index = str.FindSecondIndex("World");
+Console.WriteLine(index);
+int sum = str.CountWordsStartingWithUpper();
+Console.WriteLine(sum);
+str.ReplaceWordsWithNewWord("hi");
+Console.WriteLine(str);
---
+++ HomeTask3/hometask.3.2/Strings/StringClass.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings
+{
+    internal class StringClass
+    {
+        private string _str;
+        public StringClass(string str="")
+        {
+            _str= str;
+        }
+        public int? FindSecondIndex(string substring)
+        {
+           int index= 0;
+           index = _str.IndexOf(substring, _str.IndexOf(substring) + substring.Length);
+            if (index == -1)
+                return null;
+            return index;
+        }
+        public int CountWordsStartingWithUpper()
+        {
+            string[] substrings = _str.Split(new char[] { ' ', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach (var sb in substrings)
+            {
+                if (char.IsUpper(sb[0]))
+                    count++;
+            }
+            return count;
+        }
+        public void ReplaceWordsWithNewWord(string substring)
+        {
+
+            string[] substrings = _str.Split(new char[] { ' ', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            string str;
+            for (int i = 0; i < substrings.Length; i++)
+            {
+                str = substrings[i];
+                for (int j = 1; j < str.Length; j++)
+                {
+                    if (char.ToLower(str[j - 1]) == char.ToLower(str[j]))
+                    {
+                        substrings[i] = substring;
+                        break;
+                    }
+
+                }
+            }
+            _str = string.Join(' ', substrings);
+        }
+        public override string ToString()
+        {
+            return _str;
+        }
+    }
+}
