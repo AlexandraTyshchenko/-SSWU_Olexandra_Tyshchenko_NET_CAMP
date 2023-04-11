e399ad5c5ebfd1ff0f352eca0f2d21e98c568158
senivlm <91743906+senivlm@users.noreply.github.com>
2023-04-11 16:17:50 +0300

    Update Program.cs

--- HomeTask3/hometask.3.2/Strings/Program.cs
+++ HomeTask3/hometask.3.2/Strings/Program.cs
@@ -1,7 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
+// Не побачила малюнка з діаграмою. Домовлялись про файл в форматі малюнка теж...
+// See https://aka.ms/new-console-template for more information
 using Strings;
 StringClass str = new StringClass("Hello World Hello World World");
 int? index = str.FindSecondIndex("World");
+// А якщо індекс null, як друкувати будемо?
 Console.WriteLine(index);
 int sum = str.CountWordsStartingWithUpper();
 Console.WriteLine(sum);
