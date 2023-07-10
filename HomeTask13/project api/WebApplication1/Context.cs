04fc43fc60ef6a38bb6068638b8169d5fff2effb
oleksandratisenko59 <oleksandratisenko59@gmail.com>
2023-07-10 23:54:19 +0300

    task13

---
+++ HomeTask13/project api/WebApplication1/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApplication1.Entities;
+
+namespace WebApplication1
+{
+    public class Context:DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+
+        }
+        public DbSet<User> users { get; set; }
+        public DbSet<Word> words { get; set; }
+        public DbSet<Translate> languages { get; set; }
+        public DbSet<WordCollection> wordsCollection { get; set; }
+        public DbSet<SupportedLanguage> supportedLanguages { get; set; }
+     
+        public DbSet<WordAndWordCollection> WordAndWordCollection { get; set; }
+    }
+}
---
+++ HomeTask13/project api/WebApplication1/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApplication1.Entities
+{
+    public class User:BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public ICollection<WordCollection> wordCollections { get; set; }= new List<WordCollection>();
+        //буде зв'язок 1 до багатьох а не багато до багатьох оскільки кожен юзер буде створювати свій збірник
+        
+    }
+}
